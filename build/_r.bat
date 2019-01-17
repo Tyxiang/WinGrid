06e31448b6b847272c3d2c475a4ff61ee4cfdc6b
Tyxiang <en1434@gmail.com>
2019-01-17 13:01:32 +0800

    rebulid dir

---
+++ build/_r.bat
@@ -0,0 +1,7 @@
+"D:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /icon ..\icon.ico /in ..\WinGrid.ahk /out WinGrid.exe
+
+_7z.exe a WinGrid.zip *.* -x@_x.ini
+
+_u.bat
+
+pause
---
+++ build/_u.bat
@@ -0,0 +1,7 @@
+taskkill -im WinGrid.exe
+
+_7z.exe e WinGrid.zip -o"D:\Program Files\WinGrid\" -y
+
+Start "" "D:\Program Files\WinGrid\WinGrid.exe"
+
+pause
